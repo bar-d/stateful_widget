064d636981113a3e67d2ce073adf26bd1500c337
bard <schy2503@naver.com>
2023-06-08 23:14:47 +0900

    feat: State를 통한 UI 상태 관리

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,39 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  List<int> numbers = [];
+
+  void onClicked() {
+    setState(() {
+      counter += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
-        body: Container(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Click Count",
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                "$counter",
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
